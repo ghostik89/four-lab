755d7f26cd8f932d62fa2048da6605b93b334231
Lady_Proger <facktartar@mail.com>
2020-05-16 19:44:02 +0400

    Updates

---
+++ precache-manifest.6e48b45ae05bbb85c8a6f457c42f2756.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ff0da400e5a030ec9b36cc0b274165",
+    "url": "/four-lab/index.html"
+  },
+  {
+    "revision": "72f18f1ac8c1c9878984",
+    "url": "/four-lab/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e4188359248eb2d2bf90",
+    "url": "/four-lab/static/js/2.8d689955.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/four-lab/static/js/2.8d689955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f18f1ac8c1c9878984",
+    "url": "/four-lab/static/js/main.d455502c.chunk.js"
+  },
+  {
+    "revision": "b89bb2f317f6e6bf6a0e",
+    "url": "/four-lab/static/js/runtime-main.5462e8a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/four-lab/static/media/logo.5d5d9eef.svg"
+  }
+]);
